bb1c6e688d0a50f41099e5d22504b53c7605f709
AnabelHuancaM <91922240+AnabelHuancaM@users.noreply.github.com>
2023-09-01 19:27:30 -0500

    subiendo cambios

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './Header.js';
+import Banner from './Banner.js';
+const App = () =>{
+  return (
+    <>
+    <h1>Hola</h1>
+    <Header/>
+    <Banner/>
+    </>
+  )
+}
+
+export default App;
