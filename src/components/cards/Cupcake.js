3548cf59703ff7136fede379ecf5f5375e748970
AnabelHuancaM <91922240+AnabelHuancaM@users.noreply.github.com>
2023-09-04 12:37:15 -0500

    update components

---
+++ src/components/cards/Cupcake.js
@@ -0,0 +1,22 @@
+const Cupcake = ({
+    descripcion, 
+    sabor, 
+    imagen, 
+    color, 
+    precio
+}) => {
+    return(
+        <div className="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div className="card" >
+                <img src={imagen} className="card-img-top" alt={sabor} />
+                <div className="card-body">
+                    <p className="card-text">{descripcion}</p>
+                    <p className="card-text">Color: {color}</p>
+                    <p className="card-text">Precio:{precio}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cupcake
--- src/components/pages/cupcakes.js
+++ src/components/pages/cupcakes.js
@@ -1,8 +1,43 @@
+import { useEffect, useState } from 'react';
+import Cupcake from '../cards/Cupcake.js';
+
 const Cupcakes = () => {
+
+    const [cupcakes, setCupcakes] = useState()
+    useEffect( () => {// traer todos los datos
+        fetch("http://localhost:3001/cupcakes")
+        .then(response => response.json()) //respuesta a json
+        .then(data => setCupcakes(data)) //cupcakes tendra el valor de data
+    }, [])
+
     return(
-        <div>
-            <h1>Hola</h1>
-        </div>
+    <div className="container">
+        <h1>Página de Cupcakes</h1>
+        {
+        cupcakes ? (
+            <section className='row gy-4'>{
+                cupcakes.map(({
+                    id,
+                    descripcion, 
+                    sabor, 
+                    imagen, 
+                    color, 
+                    precio
+                }) => (
+                    <Cupcake 
+                    key={id}
+                    imagen={imagen} 
+                    descripcion={descripcion}
+                    sabor={sabor}
+                    color={color}
+                    precio={precio}/>
+                ))
+            }</section>
+            
+        ) : (<span>Cargando....</span>)
+        }
+    </div>
+        
     )
 }
 
