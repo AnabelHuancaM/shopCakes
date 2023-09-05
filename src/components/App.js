67f1bf0991f3315836fce47b3ad952c3372fb40b
AnabelHuancaM <91922240+AnabelHuancaM@users.noreply.github.com>
2023-09-05 09:02:22 -0500

    add request

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Footer from './sections/Footer.js';
 import Home from './pages/home.js';
 import Cupcakes from './pages/cupcakes.js';
 import AboutUs from './pages/aboutUs.js';
+import {estilos} from '../estilos.css';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 const App = () =>{
@@ -11,7 +12,7 @@ const App = () =>{
     <BrowserRouter>
     <Header/>
     <Routes>
-        <Route path='/cupcakes' element={ <Cupcakes/> } />
+        <Route path='/cupcakes' element={ <Cupcakes peticion="cupcakes" /> } />
         <Route path='/nosotros' element={ <AboutUs/> } />
         <Route path='/' element={ <Home/>} /> 
 
--- src/components/pages/cupcakes.js
+++ src/components/pages/cupcakes.js
@@ -1,18 +1,19 @@
 import { useEffect, useState } from 'react';
 import Cupcake from '../cards/Cupcake.js';
 
-const Cupcakes = () => {
+const Cupcakes = ( {peticion} ) => {
 
     const [cupcakes, setCupcakes] = useState()
+
     useEffect( () => {// traer todos los datos
-        fetch("http://localhost:3001/cupcakes")
+       fetch(`http://localhost:3050/${peticion}`)
         .then(response => response.json()) //respuesta a json
         .then(data => setCupcakes(data)) //cupcakes tendra el valor de data
     }, [])
 
     return(
     <div className="container">
-        <h1>Échale un vistazo a nuestros cupcakes</h1>
+        <h2>Échale un vistazo a nuestros cupcakes</h2>
         {
         cupcakes ? (
             <section className='row gy-4'>{
